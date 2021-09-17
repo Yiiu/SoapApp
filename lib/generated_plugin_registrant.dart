6679e536296adc3c9a2f407d636191af7a8cb73b
yiiu <yu.gintoki@gmail.com>
2021-09-17 17:30:51 +0800

    chore: update flutter 2.5

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -2,6 +2,7 @@
 // Generated file. Do not edit.
 //
 
+// ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
 import 'package:connectivity_for_web/connectivity_for_web.dart';
@@ -15,6 +16,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
+  // BlurhashPlugin.registerWith(registrar);
   ConnectivityPlugin.registerWith(registrar);
   PackageInfoPlugin.registerWith(registrar);
   SentryFlutterWeb.registerWith(registrar);
