0b761b2acfbe125c664daa4397b570f4e8fb908c
yiiu <yu.gintoki@gmail.com>
2021-09-17 17:32:14 +0800

    chore: update

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,6 +5,7 @@
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 
+import 'package:blurhash_web/blurhash_web.dart';
 import 'package:connectivity_for_web/connectivity_for_web.dart';
 import 'package:package_info_plus_web/package_info_plus_web.dart';
 import 'package:sentry_flutter/sentry_flutter_web.dart';
@@ -16,7 +17,7 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
-  // BlurhashPlugin.registerWith(registrar);
+  BlurhashPlugin.registerWith(registrar);
   ConnectivityPlugin.registerWith(registrar);
   PackageInfoPlugin.registerWith(registrar);
   SentryFlutterWeb.registerWith(registrar);
