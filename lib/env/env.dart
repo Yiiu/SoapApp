f87e4e1bbce96415d766946e75d2efb424ce8e8a
Yiiu <yu.gintoki@gmail.com>
2024-01-20 15:12:59 +0800

    fix: bug

--- lib/env/env.dart
+++ lib/env/env.dart
@@ -4,24 +4,24 @@ part 'env.g.dart';
 
 @Envied()
 abstract class Env {
-  @EnviedField(varName: 'BASIC_TOKEN')
+  @EnviedField(varName: 'BASIC_TOKEN', defaultValue: '')
   static const String basicToken = _Env.basicToken;
 
-  @EnviedField(varName: 'API_URL')
+  @EnviedField(varName: 'API_URL', defaultValue: '')
   static const String apiUrl = _Env.apiUrl;
 
-  @EnviedField(varName: 'SENTRY_URL')
+  @EnviedField(varName: 'SENTRY_URL', defaultValue: '')
   static const String sentryUrl = _Env.sentryUrl;
 
-  @EnviedField(varName: 'OSS_PREFIX_PATH')
+  @EnviedField(varName: 'OSS_PREFIX_PATH', defaultValue: '')
   static const String ossPrefixPath = _Env.ossPrefixPath;
 
-  @EnviedField(varName: 'OAUTH_GITHUB_CLIENT_ID')
+  @EnviedField(varName: 'OAUTH_GITHUB_CLIENT_ID', defaultValue: '')
   static const String oauthGithubClientId = _Env.oauthGithubClientId;
 
-  @EnviedField(varName: 'OAUTH_GOOGLE_CLIENT_ID')
+  @EnviedField(varName: 'OAUTH_GOOGLE_CLIENT_ID', defaultValue: '')
   static const String oauthGoogleClientId = _Env.oauthGoogleClientId;
 
-  @EnviedField(varName: 'OAUTH_WEIBO_CLIENT_ID')
+  @EnviedField(varName: 'OAUTH_WEIBO_CLIENT_ID', defaultValue: '')
   static const String oauthWeiboClientId = _Env.oauthWeiboClientId;
 }
